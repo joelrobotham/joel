92b4a039a9518430056521665264d4da736aec29
Joel Robotham <joelr@joel-robotham.local>
2011-06-29 22:05:34 +1000

    now generate the warfile rebel stuff

--- buildSrc/src/main/groovy/JRebelPlugin.groovy
+++ buildSrc/src/main/groovy/JRebelPlugin.groovy
@@ -4,30 +4,60 @@ import org.gradle.api.Plugin
 class JRebelPlugin implements Plugin<Project> {
   def void apply(Project project) {
 
-    project.task('copyJrebel') << {
-       println "creating jrebel file"
+    project.task('copyJRebelJarXml') << {
+       println "creating jrebel jar config"
        def file = """<?xml version="1.0" encoding="UTF-8"?>
 <application
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xmlns="http://www.zeroturnaround.com"
   xsi:schemaLocation="http://www.zeroturnaround.com http://www.zeroturnaround.com/alderaan/rebel-2_0.xsd">
   <classpath>
-    <dir name="${project.rootDir}/bin"/>
-    <dir name="${project.rootDir}/src/main/resources"/>
+    <dir name="${project.projectDir}/bin"/>
+    <dir name="${project.projectDir}/src/main/resources"/>
   </classpath>
 </application>"""
        project.file("build/classes/main/rebel.xml").append(file)
     }
 
-    project.jar.doFirst {
-      project.copyJrebel.execute()
+    project.copyJRebelJarXml.group = 'build'
+    project.copyJRebelJarXml.description = 'Generated a jrebel.xml file and put it in the build/main/classes directory, ready to be jarred up'
+
+    project.task('copyJRebelWarXml') << {
+      println "creating jrebel war config"
+      def fileContents = """<?xml version="1.0" encoding="UTF-8"?>
+<application
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xmlns="http://www.zeroturnaround.com"
+  xsi:schemaLocation="http://www.zeroturnaround.com http://www.zeroturnaround.com/alderaan/rebel-2_0.xsd">
+  <classpath>
+    <dir name="${project.projectDir}/bin"/>
+  </classpath>
+  <web>
+    <link target="/">
+      <dir name="${project.projectDir}/src/main/webapp"/>
+    </link>
+  </web>
+</application>"""
+     project.file("build/classes/main/rebel.xml").append(fileContents)
     }
-    project.copyJrebel.group = 'build'
-    project.copyJrebel.description = 'Generated a jrebel.xml file and put it in the build/main/classes directory, ready to be jarred up'
+   
+    project.copyJRebelWarXml.group = 'build'
+    project.copyJRebelWarXml.description = 'Generated a jrebel.xml file and put it in the build/main/classes directory, ready to be jarred up'
+
 
     project.gradle.taskGraph.whenReady {taskGraph ->
-      if (taskGraph.hasTask(':api:release')) {
-          project.copyJrebel.enabled = false
+      if (!taskGraph.hasTask(':api:release')) {
+          if (project.tasks.findByName('jar') != null) {
+            project.jar.doFirst {
+              project.copyJRebelJarXml.execute()
+            } 
+          }
+          if (project.tasks.findByName('war') != null) {
+            project.war.doFirst {
+              project.copyJRebelWarXml.execute()
+            } 
+          }
+
       }
   }
   }
