34d2b11e8bb5f6efd8ad2b9ace5bac47c009d14b
Joel Robotham <joelr@joel-robotham.local>
2011-07-19 23:46:59 +1000

    extract the template code out into the resources directory

--- buildSrc/src/main/groovy/JRebelPlugin.groovy
+++ buildSrc/src/main/groovy/JRebelPlugin.groovy
@@ -1,22 +1,20 @@
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import groovy.text.*
 
 class JRebelPlugin implements Plugin<Project> {
   def void apply(Project project) {
 
     project.task('copyJRebelJarXml') << {
        println "creating jrebel jar config"
-       def file = """<?xml version="1.0" encoding="UTF-8"?>
-<application
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xmlns="http://www.zeroturnaround.com"
-  xsi:schemaLocation="http://www.zeroturnaround.com http://www.zeroturnaround.com/alderaan/rebel-2_0.xsd">
-  <classpath>
-    <dir name="${project.projectDir}/bin"/>
-    <dir name="${project.projectDir}/src/main/resources"/>
-  </classpath>
-</application>"""
-       project.file("build/classes/main/rebel.xml").append(file)
+       def template = getClass().getResourceAsStream("jrebel-jar-template.xml").text
+       def binding = [projectDir: project.projectDir]
+       def fileContents = new GStringTemplateEngine().createTemplate(template).make(binding).toString()
+       def rebelXmlTarget = project.file("build/classes/main/rebel.xml")
+       def w = rebelXmlTarget.newWriter() 
+       w << fileContents
+       w.flush()
+       w.close()
     }
 
     project.copyJRebelJarXml.group = 'build'
