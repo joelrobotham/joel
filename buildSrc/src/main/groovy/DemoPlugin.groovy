2dd34c3e8ce22fd99cb1babbcaad96bd27cda214
Joel Robotham <joelr@joel-robotham.local>
2011-08-16 23:36:24 +1000

    update to add a convention into the DemoPlugin and do a spike to allow a dependency project to be injected in to the plugin

--- buildSrc/src/main/groovy/DemoPlugin.groovy
+++ buildSrc/src/main/groovy/DemoPlugin.groovy
@@ -1,16 +1,41 @@
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.Task
+import org.gradle.api.tasks.bundling.Jar
 
 class DemoPlugin implements Plugin<Project> {
 
     void apply(Project project) {
 
+      def demoConvention = new DemoPluginConvention(project);
+      project.convention.plugins.demo = demoConvention
+
       project.task('taskWithProperties') << {
         println "This task has a property property1 = ${property1}"
+        println "should be using the jar with ${demoConvention.dependencyTask.archivePath}"
       }
 
       project.taskWithProperties.property1 = "ORIGINAL"
     }
 
+}
+
+class DemoPluginConvention {
+    private Project project
+    private Task dependencyTask
+
+    public DemoPluginConvention(Project project)  {
+        this.project = project
+    }
+
+    void setDependencyProject(Project otherProject) {
+        project.taskWithProperties.dependsOn otherProject.assemble 
+        dependencyTask = otherProject.jar
+    }
+
+    def demo(Closure closure) {
+        closure.delegate = this
+        closure()
+    }
 }
 
